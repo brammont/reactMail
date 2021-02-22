9bafd276666f2f976cd0df4b6070f551a3470b47
brammont <brayanmazuera1@gmail.com>
2021-02-22 16:25:18 -0500

    adding email. js

--- app/src/components/Contactme.js
+++ app/src/components/Contactme.js
@@ -1,41 +1,31 @@
-import emailjs from "emailjs-com";
 import React from 'react';
+import emailjs from 'emailjs-com';
+
+
 
 export default function ContactUs() {
 
   function sendEmail(e) {
     e.preventDefault();
 
-    emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target, 'YOUR_USER_ID')
+    emailjs.sendForm('service_eqtlenj', 'template_rdjdngf', e.target, 'user_TcLWCFgKMgYUHBHO3nQrY')
       .then((result) => {
           console.log(result.text);
       }, (error) => {
           console.log(error.text);
       });
   }
-    return(
-        <div>
-            <div className="container">
-            <form onSubmit={sendEmail}>
-                    <div className="row pt-5 mx-auto">
-                        <div className="col-8 form-group mx-auto">
-                            <input type="text" className="form-control" placeholder="Name" name="name"/>
-                        </div>
-                        <div className="col-8 form-group pt-2 mx-auto">
-                            <input type="email" className="form-control" placeholder="Email Address" name="email"/>
-                        </div>
-                        <div className="col-8 form-group pt-2 mx-auto">
-                            <input type="text" className="form-control" placeholder="Subject" name="subject"/>
-                        </div>
-                        <div className="col-8 form-group pt-2 mx-auto">
-                            <textarea className="form-control" id="" cols="30" rows="8" placeholder="Your message" name="message"></textarea>
-                        </div>
-                        <div className="col-8 pt-3 mx-auto">
-                            <input type="submit" className="btn btn-info" value="Send Message"></input>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </div>
-    )
+
+  return (
+    <form className="contact-form" onSubmit={sendEmail}>
+      <input type="hidden" name="contact_number" />
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+  );
 }
